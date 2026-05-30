a7fa73c1c9327f93269cc041a4d23d0da2edbf6e
samidadfr-ops <samidadfr@gmail.com>
2026-05-31 00:25:25 +0430

    Update server.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+
 const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("SMS Server Running");
+});
+
+app.post("/sms", (req, res) => {
+    console.log("SMS Received:");
+    console.log(req.body);
+
+    res.json({
+        success: true,
+        message: "SMS saved"
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Server is LIVE on Render 🚀");
-});
-
 app.listen(PORT, () => {
-    console.log("Server running on port", PORT);
+    console.log(`Server running on port ${PORT}`);
 });
