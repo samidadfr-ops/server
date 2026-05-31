2d0ad4a960c7b7ead7fa4074b7aee9f391e6f508
samidadfr-ops <samidadfr@gmail.com>
2026-05-31 23:56:41 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+// ثبت دستگاه
 
 app.post("/register-device", async (req, res) => {
 
-  const { deviceId, deviceName, androidVersion } = req.body;
+    const { deviceId, deviceName, androidVersion } = req.body;
 
-  try {
-    await pool.query(
-      "INSERT INTO devices(device_id, device_name, android_version) VALUES($1,$2,$3)",
-      [deviceId, deviceName, androidVersion]
-    );
+    try {
 
-    res.json({ success: true });
+        await pool.query(
+            `INSERT INTO devices
+            (device_id, device_name, android_version)
+            VALUES ($1,$2,$3)`,
+            [deviceId, deviceName, androidVersion]
+        );
 
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false });
-  }
+        res.json({
+            success: true
+        });
+
+    } catch (err) {
+
+        console.log(err);
+
+        res.status(500).json({
+            success: false
+        });
+    }
+});
+
+// تست سرور
+
+app.get("/", (req, res) => {
+
+    res.json({
+        status: "Server Online"
+    });
+});
+
+app.listen(process.env.PORT || 10000, () => {
+    console.log("Server running on port 10000");
 });
